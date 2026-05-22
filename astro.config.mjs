94d846198770c9874d72da0100afd3c5ee035e66
david garcia <dvd.vasko@gmail.com>
2026-05-22 15:00:13 +0200

    perf: forzar inlined css para eliminar peticiones bloqueantes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arenalelchiringuito.es',
+  build: {
+    inlineStylesheets: 'always'
+  },
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
